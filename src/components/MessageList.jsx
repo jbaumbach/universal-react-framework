d7cce8cedd4b3710ef8f7778fc99ffc8b5bc9cdb
John Baumbach <john.j.baumbach@gmail.com>
2016-01-10 20:23:16 +0000

    More efficient item adding; item removing; uses object vs. array for message list

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -12,33 +12,50 @@ class MessageList extends React.Component {
     super(props);
     
     this.state = {
-      messages: []
+      messages: {}
     }
     
     this.fb_Messages = new Firebase('react-demo-ergosoft.firebaseIO.com/messages');
     
     //
-    // Grab initial data, and keep grabbing changes as well
+    // Listen for new message
     //
-    this.fb_Messages.on("value", (dataSnapshot) => {
-      var messagesVal = dataSnapshot.val();
-      var messages = _.chain(messagesVal)
-        .keys()
-        .filter((key) => {
-          return messagesVal[key].message;
-        })
-        .map((key) => {
-          return _.extend(messagesVal[key], { key: key });
-        })
-        .value();
-      this.setState({
-        messages: messages
+    this.fb_Messages.on("child_added", (newMsg) => {
+      //
+      // Make sure there are no duplicates or invalid items
+      //
+      if (this.state.messages[newMsg.key()] || !newMsg.val().message) {
+        return;
+      }
+      
+      //
+      // We're not trying to mutate the state here, just a shortcut.  Let's stick
+      // the primary key into the object itself, so it's easier to get when looping.
+      //
+      let newItem = _.extend(newMsg.val(), {
+        key: newMsg.key()
       });
-    });    
+      this.state.messages[newItem.key] = newItem;
+      
+      //
+      // Set new items
+      //
+      this.setState(this.state);
+    });
+
+    //
+    // If an item is removed
+    //
+    this.fb_Messages.on("child_removed", (removedMsg) => {
+      delete this.state.messages[removedMsg.key()];
+      this.setState(this.state);
+    });
+
+
   }
   
   render() {
-    var messageNodes = this.state.messages.map((msg) => {
+    var messageNodes = _.values(this.state.messages).map((msg) => {
       return (
         <Message key={msg.key} message={msg.message} profilePic={msg.profilePic} />
       );
